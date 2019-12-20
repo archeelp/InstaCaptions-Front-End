6cb26cc466ab23ab2b5ca325009c5e61f3f30555
Saif <saif1204kazi@gmail.com>
2019-12-20 11:32:58 +0530

    Added app icon and splash screen logo

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -11,7 +11,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    new Future.delayed(const Duration(milliseconds: 200), () {
+    new Future.delayed(const Duration(milliseconds: 300), () {
       setState(() {
         show = true;
       });
@@ -29,14 +29,15 @@ class _SplashScreenState extends State<SplashScreen> {
           alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-//              Image.asset(
-//                'android/assets/images/splash_logo.png',
-//                fit: BoxFit.contain,
-//              ),
+              Image.asset(
+                'android/assets/images/splash_logo.png',
+                fit: BoxFit.contain,
+              ),
               AnimatedOpacity(
                 opacity: show ? 1.0 : 0.0,
-                duration: Duration(milliseconds: 700),
+                duration: Duration(milliseconds: 800),
                 child: Container(
                   alignment: Alignment.center,
                   child: Text(
