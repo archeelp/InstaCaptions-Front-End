8604707e767294bd7f783d6e887b18cd87efd117
Archeel Parekh <archeelparekh11@gmail.com>
2020-01-04 22:59:03 +0530

    Update scrape.py

--- BackEnd/scrape.py
+++ BackEnd/scrape.py
@@ -3,23 +3,33 @@ import time
 import selenium.webdriver as webdriver
 from bs4 import BeautifulSoup
 import os
-# def cleaner(captions_tags):
-#     tags = set()
-#     caption = ""
-#     for ct in captions_tags:
-#         if '@' in ct:
-#             ...
-#         elif '#' in ct:
-#             tags.add(ct.replace('#',''))
-#         else:
-#             caption += ct + ' '
-#     return [caption,tags]
+def cleaner(captions_tags):
+    ans = []
+    for cts in captions_tags:
+        tags = set()
+        caption = ""
+        for ct in cts:
+            if '@' in ct:
+                ...
+            elif '#' in ct:
+                tags.add(ct.replace('#',''))
+            else:
+                caption += ct + ' '
+        ans.append([caption,tags])
+    return ans
+
+def hashstr(s):
+    st = ''
+    for x in s:
+        st += x + ' '
+    return st
+
 quotes,images,links,captions_tags = [],[],[],[]
 print("Enter Usernames of the accounts to be scraped")
 user_names = [ _ for _ in input().split()]
 driver = webdriver.Chrome("/Users/saifkazi/Downloads/chromedriver")
 ig = "https://www.instagram.com/"
-f = open('data.csv','w')
+f = open('data.csv','w',encoding="utf-8")
 fh = open('urls.csv','w')
 rows = []
 urls = []
@@ -32,8 +42,6 @@ for user in user_names:
     for _ in range(5 if n > 5 else n ):
         time.sleep(1)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # response = requests.get(url)
-    # soup = BeautifulSoup(response.text, 'html.parser')
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     temp = soup.find_all("div","KL4Bh")
     for t in temp:
@@ -53,9 +61,9 @@ for user in user_names:
         if a == "Verified":
             a = temp.find_all("span")[1].get_text()
         captions_tags.append(a) if temp else captions_tags.append("")
-# captions, tags = cleaner(captions_tags)
+captions_tags = cleaner(captions_tags)
 for i in range(len(images)):
-    row = quotes[i] + "\t" + captions_tags[i] + "\n"
+    row = quotes[i] + "\t" + captions_tags[i][0] +  "\t" + hashstr(captions_tags[i][1]) +"\n"
     url = images[i] + "\n"
     rows.append(row)
     urls.append(url)
